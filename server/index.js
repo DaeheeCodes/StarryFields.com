03fb69f3249bb78a4a34a4880350666f11e2c38a
DaeheeCodes <DaeheeCodes@gmail.com>
2022-05-16 13:07:09 -0700

    route

--- server/index.js
+++ server/index.js
@@ -46,6 +46,8 @@ mongoose
 
 
 
+const PORT = process.env.PORT || 3001;
+app.set("port", PORT)
 
 
 // Have Node serve the files for our built React app
@@ -92,8 +94,9 @@ console.log(process.env.SENDGRID_API_KEY)
     })
 
 
-app.listen(process.env.PORT || 3001, () => {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    const port = process.env.PORT || 3001;
+    const server = app.listen(port, () => {
+        console.log('Connected to port ' + port)
     })
     
     // Error Handling
